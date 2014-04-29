af4e6c1d78160d3ac22a1f27c1a4e0d8b88ba90a
Andrew Stewart <andrew@stwrt.ca>
2014-04-29 16:09:08 -0700

    Correct model validations to conform to new Sequelize

--- models/tessel.js
+++ models/tessel.js
@@ -3,6 +3,6 @@
 module.exports = function Tessel(sequelize, DataTypes) {
   return sequelize.define('Tessel', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, unique: true, primaryKey: true },
-    device_id: { type: DataTypes.INTEGER, unique: true, validate: { notNull: true } },
+    device_id: { type: DataTypes.INTEGER, unique: true, allowNull: false },
   });
 };
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@
 module.exports = function User(sequelize, DataTypes) {
   return sequelize.define('User', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, unique: true, primaryKey: true },
-    auth_id: { type: DataTypes.INTEGER, unique: true, validate: { notNull: true } },
-    api_key: { type: DataTypes.STRING, unique: true, validate: { notNull: true } }
+    auth_id: { type: DataTypes.INTEGER, unique: true, allowNull: false },
+    api_key: { type: DataTypes.STRING, unique: true, allowNull: false }
   });
 };
