ba118f9119ff80a9d135a8b5292fefca1925fe3b
Andrew Stewart <andrew@stwrt.ca>
2014-04-29 09:46:37 -0700

    Check for API key presence on all requests
    This addition checks for the presence of an API key on all requests. It allows
    requests if:
    
      - the API key is in the Authorization header (works for all requests)
      - the API key is in the query params (only for GET requests)
      - the API key is in the POST body (POST requests only)
    
    Otherwise, an appropriate JSON error message and a 400 status code is returned.

--- app.js
+++ app.js
@@ -35,6 +35,43 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+// intercept and return error for all requests that don't contain an API key
+app.use(function(req, res, next) {
+  var authorized = false;
+
+  var json = {
+    code: 400,
+    error: "invalid_request",
+    error_description: "The API key was not found"
+  };
+
+  // check in Authorization header
+  if (/Bearer \w+/.test(req.headers["Authorization"])) {
+    authorized = true;
+  }
+
+  // check URL query string (only with GET requests)
+  if (req.method === "GET") {
+    if (req.query.api_key !== undefined && /\w+/.test(req.query.api_key)) {
+      authorized = true;
+    }
+  }
+
+  // check POST body
+  if (req.method === "POST") {
+    if (req.body.api_key !== undefined && /\w+/.test(req.body.api_key)) {
+      authorized = true;
+    }
+  }
+
+  if (authorized) {
+    next();
+  } else {
+    res.status = 400;
+    res.json(json);
+  }
+});
+
 app.use('/', routes.index);
 
 /// catch 404 and forwarding to error handler
