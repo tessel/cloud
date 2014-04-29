2666b6963ae096896921636532eca3be281d8a70
Andrew Stewart <andrew@stwrt.ca>
2014-04-29 14:46:31 -0700

    Add #getAPIKey method to extract it from request

--- routes/v1.js
+++ routes/v1.js
@@ -1,35 +1,43 @@
 var router = require('express').Router();
 
-// intercept and return error for all requests that don't contain an API key
-router.get("/*", function(req, res, next) {
-  var authorized = false;
-
-  var json = {
-    code: 400,
-    error: "invalid_request",
-    error_description: "The API key was not found"
-  };
+// extracts an API key (if present) from a request
+var getAPIKey = function(req) {
+  var headerRegex = /^Bearer (\w+)/;
 
   // check in Authorization header
-  if (/Bearer \w+/.test(req.headers.authorization)) {
-    authorized = true;
+  if (headerRegex.test(req.headers.authorization)) {
+    return req.headers.authorization.match(headerRegex)[1];
   }
 
   // check URL query string (only with GET requests)
   if (req.method === "GET") {
     if (req.query.api_key !== undefined && /\w+/.test(req.query.api_key)) {
-      authorized = true;
+      return req.query.api_key;
     }
   }
 
   // check POST body
   if (req.method === "POST") {
     if (req.body.api_key !== undefined && /\w+/.test(req.body.api_key)) {
-      authorized = true;
+      return req.body.api_key;
     }
   }
 
-  if (authorized) {
+  return false;
+};
+
+// intercept and return error for all requests that don't contain an API key
+router.get("/*", function(req, res, next) {
+  var json = {
+    code: 400,
+    error: "invalid_request",
+    error_description: "The API key was not found"
+  };
+
+  var key = getAPIKey(req);
+
+  if (key) {
+    req.api_key = key;
     next();
   } else {
     res.status = 400;
@@ -37,8 +45,11 @@ router.get("/*", function(req, res, next) {
   }
 });
 
-router.get("/", function(req, res) {
-  res.json({ greeting: "Hello World" });
+// # GET /v1/tessels
+//
+// Lists all tessels belonging to the currently authenticated user
+router.get("/devices", function(req, res) {
+  res.json({ api_key: req.api_key });
 });
 
 module.exports = router;
