16fe9ffb4637898fe317a280b7f12bf03fc72bdf
edgarsilva <edgar.osc@gmail.com>
2014-05-19 12:19:33 -0500

    Update oauth config with hardcoded strings for oauth paths.

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ require('dotenv').load();
 
 module.exports = {
   "server": process.env.OAUTH_SERVER,
-  "authorisePath": process.env.OAUTH_AUTH_PATH,
-  "tokenPath": process.env.OAUTH_TOKEN_PATH,
-  "profilePath": process.env.OAUTH_PROFILE_PATH
+  "authorisePath": "oauth/authorise",
+  "tokenPath": "oauth/token",
+  "profilePath": "users/profile"
 }
